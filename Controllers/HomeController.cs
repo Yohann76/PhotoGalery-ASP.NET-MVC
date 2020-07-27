66da0d27bc1512aca7a82cf70e2c13bd172d88b4
Yohann76 <yohanndurand76@gmail.com>
2020-07-27 14:55:03 +0200

    [Start] add file ASP.NET MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoGalery.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+        public ActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        // return Hello World
+        public string Index()
+        {
+            return "Hello World MVC, press F5 for result";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
